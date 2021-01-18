ed38861cfabbb5d2f4d6434c5664f5dadb6f00f8
galen <1149501789@qq.com>
2021-01-18 13:19:47 +0800

    完成增删改查的封装

---
+++ src/main.js
@@ -0,0 +1,25 @@
+//----1. test dom.after
+// let node1 = document.querySelector("#test")
+// let node2 = dom.create(`<div id="ttc">helle ttc</div>`)
+
+//----2. test dom.before
+// let node1 = document.querySelector("#test")
+// let node2 = dom.create(`<div id="ttc">helle ttc</div>`)
+// dom.before(node1, node2)
+
+//----3. dom.append
+// let node1 = document.querySelector("#test")
+// let node2 = dom.create(`<div id="ttc">helle ttc</div>`)
+// dom.append(node1, node2)
+
+//----4. dom.wrap
+// let node1 = document.querySelector("#test")
+// dom.wrap(node1, `<div id="ttc">helle ttc</div>`)
+
+//----5. dom.remove
+// let node1 = document.querySelector("#test")
+// console.log(dom.remove(node1))
+
+//----6. dom.empty
+let node1 = document.querySelector("#test")
+console.log(dom.empty(node1))
